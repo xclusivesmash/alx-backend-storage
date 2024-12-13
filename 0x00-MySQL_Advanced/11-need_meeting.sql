69fbfdcaa2c3551f2c0b23f36d3e43c10ac3dbc6
xclusivesmash <sbumatshiane916@gmail.com>
2024-12-13 17:49:17 +0000

    Initial commit

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,11 @@
+-- creates a view need_meeting that lists all students
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS
+    SELECT name
+        FROM students
+        WHERE score < 80 AND
+            (
+                last_meeting IS NULL
+                OR last_meeting < SUBDATE(CURRENT_DATE(), INTERVAL 1 MONTH)
+            )
+;
